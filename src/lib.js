43a3222705252aca2fe02bdf6bec241613461630
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-10-25 12:59:32 +0530

    added makeDeltaTracker function

--- src/lib.js
+++ src/lib.js
@@ -14,7 +14,19 @@ const makeCounterFromZero = function(){
   return makeCounterFromN(0);
 }
 
-const makeDeltaTracker = undefined;
+const makeDeltaTracker = function(oldDelta){
+  deltaTracker = {old:oldDelta,delta:0,new:oldDelta};
+  return function(delta){
+    if(delta){
+      deltaTracker.old = deltaTracker.new;
+      deltaTracker.delta = delta;
+      deltaTracker.new = delta+deltaTracker.old;
+      return deltaTracker;
+    }
+    return deltaTracker;
+  }
+}
+
 const makeFiboGenerator = undefined;
 const makeCycler = undefined;
 const curry = undefined;
